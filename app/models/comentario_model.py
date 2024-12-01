77e6c3ab299d6a4bb43f7409677ce7bb8ee32467
Joel Saravia <65045163+josaraviam@users.noreply.github.com>
2024-12-01 17:44:20 -0600

    changes

--- app/models/comentario_model.py
+++ app/models/comentario_model.py
@@ -1,10 +1,10 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from datetime import datetime
 from typing import List
 
 
 class Comentario(BaseModel):
-    id_comentario: str
+    id_comentario: str = Field(..., alias="_id")  # Mapear _id de MongoDB a id_comentario
     pnr: str
     fecha_creacion: datetime
     id_usuario: str

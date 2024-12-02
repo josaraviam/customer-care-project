ec228bcaa184db93c46af852d5fc0caec852cff5
Joel Saravia <65045163+josaraviam@users.noreply.github.com>
2024-12-01 23:45:19 -0600

    changesx

--- app/db/mongodb_connector.py
+++ app/db/mongodb_connector.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient
 from app.config import MONGO_URI
 
 mongo_client = MongoClient(MONGO_URI)
-mongo_db = mongo_client["customerCareDB"]
+mongo_db = mongo_client["customerCareComments"]
 
 usuarios_collection = mongo_db["usuarios"]
 comentarios_collection = mongo_db["comentarios"]
